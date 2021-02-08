d9b6a0b26bfa2d7ba57f3c0fe68a804babb3adde
raotaohub <1060791388@qq.com>
2021-02-08 20:21:37 +0800

    react

---
+++ React/react_staging/20_src_withRouter的使用/components/Header/index.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {withRouter} from "react-router-dom"
+
+class Header extends Component {
+  /** 普通组件的 props 身上是没有 history 属性 **/
+  /** 但是可以通过 withRouter 实现 **/
+  back = () => {
+    this.props.history.goBack()
+  }
+  forward = () => {
+    this.props.history.goForward()
+  }
+  go = () => {
+    this.props.history.go(-2)
+  }
+  /** ----------------------------------- **/
+
+  render() {
+    console.log("Header收到的props是", this.props)
+    const {props} = this
+    return (
+      <div>
+        <h5>props是{props.a}</h5>
+        <button onClick={this.back}>回退</button>
+        &nbsp;
+        <button onClick={this.forward}>前进</button>
+        &nbsp;
+        <button onClick={this.go}>g o</button>
+        &nbsp;
+      </div>
+    );
+  }
+}
+/** withRouter 可以加工一般组件，让一般组件具备路由组件才特有的 API **/
+/** @return 新组件 **/
+export default withRouter(Header);/** -- **/
+/** ----------------------------------- **/
--- React/react_staging/src/App.js
+++ React/react_staging/src/App.js
@@ -1,55 +1,30 @@
 import React from "react"
-import {Route, BrowserRouter, Switch, Redirect} from "react-router-dom";
-import Home from "./page/Home";
-import About from "./page/About"
-import Header from "./components/Header";
-import MyNavLink from "./components/MyNavLink";
-
-/** (1).导航使用 <Link to="/xx" component= {} ></Link>标签
- *  (2).展示用 <Route path="/xx"> </Route>
- *  (3). 最外层需要一个路由器 <BrowserRouter> </BrowserRouter> 或 哈希路由<HashRouter>
- * **/
+import {Button,Space, DatePicker} from 'antd';
+import {WechatOutlined} from '@ant-design/icons';
+import "antd/dist/antd.css"
 
 export default class App extends React.Component {
+  onChange = ()=>{
+    console.log("改变了改变了改变了改变了改变了")
+  }
   render() {
     return (
-      <BrowserRouter>
-        <div className="container">
-          <div>
-            <div className="row">
-              <div className="col-xs-offset-2 col-xs-8">
-                <div className="page-header"><h2>React Router Demo</h2></div>
-                <Header a={9999999}/>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-xs-2 col-xs-offset-2">
-                <div className="list-group">
-
-                  <MyNavLink to="/about">About</MyNavLink>
-                  <MyNavLink to="/home">Home</MyNavLink>
-
-                </div>
-              </div>
-              <div className="col-xs-6">
-                <div className="panel">
-                  <div className="panel-body">
-                    {/** Switch只允许进行单一匹配，最好使用该标签 **/}
-                    <Switch>
-                      <Route path="/about" component={About}/>
-                      <Route path="/home" component={Home}/>
-                      {/** 重定向 **/}
-                      <Redirect to="/about"></Redirect>
-                    </Switch>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <br/>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+        <WechatOutlined/>
+        <Space direction="vertical">
+          <DatePicker onChange={this.onChange}/>
+        </Space>
+      </div>
     )
   }
 }
+//├─
+//└─
 
 
