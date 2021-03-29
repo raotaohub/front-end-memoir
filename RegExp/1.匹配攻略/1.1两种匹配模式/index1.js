7179b287111ca06eb02b42191ddbb7bf22b0428f
raotaohub <1060791388@qq.com>
2021-03-29 23:46:44 +0800

    正则表达

---
+++ RegExp/1.匹配攻略/1.1两种匹配模式/index1.js
@@ -0,0 +1,13 @@
+var reg = /hello/;
+
+console.log(reg.test('hello world'));
+
+//
+
+/**
+ * !1.横向匹配模式 {m,n} 意味着长度不确定 可以是多种情况
+ * @description g是全局匹配 强调 所有！ 而不是第1个
+ */
+var reg = /ab{2,5}c/g; // ? 第1个字符是 "a" 接下来是2到5个 "b" 最后是 "c"
+var string = 'abc abbc abbbc abbbbc abbbbbc abbbbbbc';
+console.log(string.match(reg)); // => ["abbc", "abbbc", "abbbbc", "abbbbbc"]
