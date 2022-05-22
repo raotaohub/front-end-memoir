f8fd5fc7d799e7549869ca13ed6ca4da38a326e6
raotaohub <1060791388@qq.com>
2022-05-22 22:26:24 +0800

    desc :add

--- TypeScript/TS类型体操/tdl-ts-main/TypeScript 类型体操通关秘籍/12-类型变成综合实战一/index.ts
+++ TypeScript/TS类型体操/tdl-ts-main/TypeScript 类型体操通关秘籍/12-类型变成综合实战一/index.ts
@@ -1,6 +1,58 @@
+// ?字符串类型
+// 横杠转驼峰
 type KebabCaseToCamelCase<Str extends string> = Str extends `${infer Case}-${infer Rest}`
   ? `${Case}${KebabCaseToCamelCase<Capitalize<Rest>>}`
   : Str;
 
-type camelcase = KebabCaseToCamelCase<"aa-bb-cc">;
+type camelcase = KebabCaseToCamelCase<"aa-bb-cc-dd">;
+
+// 驼峰转横杠
+type CamelCaseToKebabCase<Str extends string> = Str extends `${infer Case}${infer Rest}`
+  ? Case extends Lowercase<Case> // 关键点在这 通过大小写 来判断是否是驼峰的分界
+    ? `${Case}${CamelCaseToKebabCase<Rest>}`
+    : `-${Lowercase<Case>}${CamelCaseToKebabCase<Rest>}`
+  : Str;
+
+type kebabcase = CamelCaseToKebabCase<camelcase>;
+
+// ?数组类型
+
+type Chunk<
+  A extends unknown[],
+  ChunkLen extends number = 1,
+  Item extends unknown[] = [],
+  Result extends unknown[] = []
+> = A extends [infer Val, ...infer Rest]
+  ? Item["length"] extends ChunkLen
+    ? Chunk<Rest, ChunkLen, [Val], [...Result, Item]> // 如果 Item 已经满足 Chunlen，那么把 Item 加入结果数组
+    : Chunk<Rest, ChunkLen, [...Item, Val], Result> // 如果 Item 没有满足，那么继续构建 Item
+  : [...Result, Item];
+
+type chunk = Chunk<[1], 3>;
+
+// ?索引类型
+/* 
+[‘a’, ‘b’, ‘c’]
+=>
+{
+  a: {
+      b: {
+          c: 'xxx'
+      }
+  } 
+}
+*/
+
+//* type a = keyof any =>  type a = string | number | symbol 取当前支持索引支持的类型
+
+type TupleToNestedObject<T extends unknown[], V> = T extends [infer Val, ...infer Rest]
+  ? {
+      [Key in Val as Key extends keyof any ? Key : never]: Rest extends unknown[]
+        ? TupleToNestedObject<Rest, V>
+        : V;
+    }
+  : V;
+
+type t = TupleToNestedObject<["a", "b", "c"], "xxx">;
+
 export {};
